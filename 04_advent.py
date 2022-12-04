85b1f4b27ec69a579d2c4cb361d74b2079d14e07
michael.vollstedt <michael.vollstedt@idealo.de>
2022-12-04 11:51:38 +0100

    2022-12-04 done

--- 04_advent.py
+++ 04_advent.py
@@ -1,50 +1,82 @@
 import string
 
 fileName = '04_advent.txt'
-backpacks_3 = []
-score = 0
+numberOfSubsets = 0
+
 
 def readFile():
-    global backpacksCompartment_1
+    pairs = []
     lines = open(fileName, 'r')
 
     for line in lines:
         line = line.strip()
-        string1 = line[:len(line)//2]
-        string2 = line[len(line)//2:]
-        backpacksCompartment_1.append(string1)
-        backpacksCompartment_2.append(string2)
-        print(backpacksCompartment_1)
-        print(backpacksCompartment_2)
+        pairs.append(line)
+
+    return pairs
+
+def separate_range_string(range_string):
+    # Split the string on the comma to get the two ranges
+    ranges = range_string.split(',')
+
+    # Create two empty lists to store our numbers
+    numbers1 = []
+    numbers2 = []
+
+    # Loop through the ranges
+    for i, r in enumerate(ranges):
+        # Split the range on the dash to get the start and end of the range
+        start, end = r.split('-')
+
+        # Convert the start and end to integers
+        start = int(start)
+        end = int(end)
+
+        # Add all the numbers in the range to the appropriate list
+        if i == 0:
+            numbers1.extend(range(start, end + 1))
+        else:
+            numbers2.extend(range(start, end + 1))
+
+    # Return the two lists of numbers
+    return numbers1, numbers2
+
 
 def compareBackpack(bp_1, bp_2):
     item = ''
     return item
 
-def firstPart():
-    global score
-    i = 0
-    readFile()
-    n = len(backpacksCompartment_1)
+def checkForSubset(pairList):
+    global numberOfSubsets
 
-    for i in range(n):
-        resultItem = compareBackpack(backpacksCompartment_1[i], backpacksCompartment_2[i])
-        print('Found identical backpack item: ' + str(resultItem))
-        scorePerItem = int(itemScore(resultItem))
-        print('score for item ' + str(resultItem) + ' is = ' + str(scorePerItem))
-        score += scorePerItem
+    for pair in pairList:
+        elfA, elfB = separate_range_string(pair)
+        if set(elfA).issubset(set(elfB)):
+            numberOfSubsets += 1
+            print('matching pair: ' + str(pair))
+        elif set(elfB).issubset(set(elfA)):
+            numberOfSubsets += 1
+            print('matching pair: ' + str(pair))
+
+def checkForIntersection(pairList):
+    global numberOfSubsets
+
+    for pair in pairList:
+        elfA, elfB = separate_range_string(pair)
+        if set(elfA).intersection(set(elfB)):
+            numberOfSubsets += 1
+            print('matching pair: ' + str(pair))
+
+def firstPart():
+    pairList = readFile()
+    checkForSubset(pairList)
 
 
 def secondPart():
-    global score
-    i = 0
-    readFile()
-
+    pairList = readFile()
+    checkForIntersection(pairList)
 
 
 if __name__ == '__main__':
-    firstPart()
-    #secondPart()
-    print('your score: ' + str(score))
-
-
+    #firstPart()
+    secondPart()
+    print('your sub-set pairs: ' + str(numberOfSubsets))
