06c75ce6ecd58f195986b0a890471163a5c45be0
michael.vollstedt <michael.vollstedt@idealo.de>
2022-12-06 22:02:15 +0100

    2022-12-06 no-brute-force added

--- 06_advent.py
+++ 06_advent.py
@@ -1,5 +1,5 @@
 import time
-from collections import deque
+from collections import deque, Counter
 fileName = '06_advent.txt'
 
 def readFile():
@@ -59,6 +59,61 @@ def get_opt_2_message_endpoint(line):
             return i
     return -1
 
+def get_window_smart_message_endpoint(line):
+    # line needs to be a string
+    i = 14
+    line_len = len(line)
+    while i <= line_len-1:
+        window = line[i-14:i]
+        if len(set(window)) == 14:
+            return i
+        else:
+            collection = Counter(window)
+            # check for multiple occurrences
+            not_unique = [value > 1 for value in collection.values()]
+            # get index of duplicates
+            dub_list = [y for y, x in enumerate(not_unique) if x == True]
+            # get char of the duplicates
+            keys = list(collection.keys())
+            alpha_list = []
+            for idx in dub_list:
+                alpha_list.append(keys[int(idx)])
+            # get all positions of char
+            key_pos_dict = {}
+            for char in alpha_list:
+                x = 0
+                found = []
+                while x < len(window):
+                    x = window.find(char, x)
+                    if x == -1:
+                        break
+                    found.append(x)
+                    x += 1
+                found.sort()
+                key_pos_dict[char] = found
+
+            # get highest index of duplicate
+            idx_dup = 0
+            high_key = ''
+            for k, v in key_pos_dict.items():
+                max_value = max(v)
+                if max_value > idx_dup:
+                    idx_dup = max_value
+                    high_key = k
+
+            # finally, get second backward position of duplicate char
+            high_value = key_pos_dict[high_key]
+            second_high = high_value[-2]
+            # always get the next index of the second last high index (might be the same,
+            # if both char are next to each-other
+            next_idx = second_high
+            if next_idx < line_len-1-14:
+                i += next_idx + 1
+                #print(i)
+            else:
+                i = line_len-1-14
+    return -1
+
 def get_deque_message_endpoint(line):
     window = deque(maxlen=14)
     for idx, char in enumerate(line, 1):
@@ -84,21 +139,21 @@ def secondPart():
     st = time.time()
     end = get_message_endpoint(line)
     et = time.time()
-    print('The end of unique code (badly implemented) = ' + str('?'))
+    print('The end of unique code (badly implemented) = ' + str(end))
     elapsed_time = (et - st) * 1000
     print('Execution time (badly implemented):', elapsed_time, 'milli-seconds')
 
     st = time.time()
     end = get_opt_message_endpoint(line)
     et = time.time()
-    print('The end of unique code (window) = ' + str('?'))
+    print('The end of unique code (window) = ' + str(end))
     elapsed_time = (et - st) * 1000
     print('Execution time (window):', elapsed_time, 'milli-seconds')
 
     st = time.time()
     end = get_opt_2_message_endpoint(line)
     et = time.time()
-    print('The end of unique code (window-optimized) = ' + str('?'))
+    print('The end of unique code (window-optimized) = ' + str(end))
     elapsed_time = (et - st) * 1000
     print('Execution time (window-optimized):', elapsed_time, 'milli-seconds')
 
@@ -106,9 +161,16 @@ def secondPart():
     st = time.time()
     end = get_deque_message_endpoint(signal)
     et = time.time()
-    print('The end of unique code (deque) = ' + str('?'))
+    print('The end of unique code (deque) = ' + str(end))
     elapsed_time = (et - st) * 1000
     print('Execution time (deque):', elapsed_time, 'milli-seconds')
+
+    st = time.time()
+    end = get_window_smart_message_endpoint(signal)
+    et = time.time()
+    print('The end of unique code (find algo) = ' + str(end))
+    elapsed_time = (et - st) * 1000
+    print('Execution time (find algo):', elapsed_time, 'milli-seconds')
 
 if __name__ == '__main__':
     #firstPart()
