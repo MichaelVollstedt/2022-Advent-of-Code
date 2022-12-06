b630f72c0d0b5b8308f862830bfe248d788cdec6
michael.vollstedt <michael.vollstedt@idealo.de>
2022-12-06 09:28:02 +0100

    2022-12-06 done

--- 06_advent.py
+++ 06_advent.py
@@ -1,23 +1,54 @@
-fileName = '05_advent.txt'
+fileName = '06_advent.txt'
 
 def readFile():
     lines = open(fileName, 'r')
 
     line = lines.readline()
-    row = [line[i * 4 + 1] for i in range(len(line) // 4)]
-    crates = [[] for i in range(len(row))]
+    str = line.strip()
+    c_list = [x for x in str]
 
-    return crates
+    return c_list
 
+def get_buffer_endpoint(line):
+    i = 0
+    y = 3 + 1
+    for c in line:
+        check_buffer = line[i:y]
+        sqnc = list(set(check_buffer))
+        if len(check_buffer) == len(sqnc):
+            break
+        else:
+            i += 1
+            y += 1
+
+    return y
+
+def get_message_endpoint(line):
+    i = 0
+    y = 13 + 1
+    for c in line:
+        check_buffer = line[i:y]
+        sqnc = list(set(check_buffer))
+        if len(check_buffer) == len(sqnc):
+            break
+        else:
+            i += 1
+            y += 1
+
+    return y
 def firstPart():
-    stacks = readFile()
+    line = readFile()
+    end = get_buffer_endpoint(line)
+    print('The end of unique code = ' + str(end))
 
 
 
 def secondPart():
-    stacks = readFile()
+    line = readFile()
+    end = get_message_endpoint(line)
+    print('The end of unique code = ' + str(end))
 
 
 if __name__ == '__main__':
-    firstPart()
-    #secondPart()
+    #firstPart()
+    secondPart()
